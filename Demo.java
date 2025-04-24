238a8c9b236b2e1cbdecfa346ca59d7085215b0a
Malagouda1863 <malangoudapatil7788@gmail.com>
2025-04-24 11:45:59 +0530

    save

--- Demo.java
+++ Demo.java
@@ -1,9 +1,7 @@
 public class Demo {
     public static void main(String[] args) {
         int n1=10,n2=5;
-        //Arithmetic operators
-        System.out.println("The sum of n1 and n2 is: " + (n1+n2));
-        System.out.println("The substraction of n1 and n2 is: " + (n1-n2)); 
+        //Arithmetic operators 
         System.out.println("The multiplition of n1 and n2 is: " + (n1*n2));
         System.out.println("The division of n1 and n2 is: " + (n1/n2));
     }
